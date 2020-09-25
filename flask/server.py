a72c4afff6e92ee5006471f4475ed31d6eec306a
Paolo Monteverdi <p.monteverdi@blockchainlab.it>
2020-09-25 16:17:44 +0200

    some fix

--- flask/server.py
+++ flask/server.py
@@ -1,14 +1,12 @@
 from flask import Flask
-from dotenv import load_dotenv
 import os
 
-load_dotenv()
 app = Flask(__name__)
-SERVER_PORT = os.getenv("SERVER_PORT")
+
 
 @app.route("/")
 def hello():
     return "Hello World!"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=SERVER_PORT)
+    app.run(host='0.0.0.0', port=8088)
