572bbf2a51db1d202d0761e3886e400060ebb468
PaolMon <41119720+PaolMon@users.noreply.github.com>
2020-09-28 17:41:35 +0200

    Update server.py

--- flask/server.py
+++ flask/server.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/home")
 def hello():
-    return "<p>Hello World!</p>"
+    return "<p><h1>Hello World!</h1></p><p>hello hello hello</p>"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
